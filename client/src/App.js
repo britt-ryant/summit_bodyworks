7e51c3fea0b9b1093efabe7dd36393f028f82552
CRHarding <casey.r.harding@gmail.com>
2018-05-29 19:32:38 -0400

    Added in map

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    require('dotenv').config();
     return (
       <div className="App">
         <header className="App-header">
